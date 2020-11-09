79ca865d548957703261ebb8feb98de94a5a70e8
loerssoni <lauri.heikka@gmail.com>
2020-11-09 22:36:18 +0200

    colab notebook

--- options/train_options.py
+++ options/train_options.py
@@ -10,6 +10,7 @@ class TrainOptions(BaseOptions):
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)
         # visdom and HTML visualization parameters
+        parser.add_argument('--log_name', default='loss_log.txt')
         parser.add_argument('--print_freq', type=int, default=100, help='frequency of showing training results on console')
         # network saving and loading parameters
         parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
