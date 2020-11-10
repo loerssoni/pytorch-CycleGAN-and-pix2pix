d0d759d1bc6e8b97e5701ddb5d507138919ee1fe
loerssoni <lauri.heikka@gmail.com>
2020-11-10 10:53:02 +0200

    add transforms

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -82,7 +82,7 @@ class Pix2PixModel(BaseModel):
 
         self.real_A = input['A'].to(self.device)
         self.real_B = input['B'].to(self.device)
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        self.image_paths = input['A_paths']
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
